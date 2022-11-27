b570d4ccba5a496a97dc670510a37aea6dbcb117
alan <cvinny33@gmail.com>
2022-11-27 15:20:57 -0700

    imported useMutation broke down and simplified lines

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
-import Auth from '../utils/auth';
+import {useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import Auth from '../utils/auth';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -67,14 +69,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      const { data } = await saveBook({
+        variables: { bookData: {...bookToSave}},
+      });
+      console.log(savedBookIds);
+      setSavedBookIds([...saveBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
     }
